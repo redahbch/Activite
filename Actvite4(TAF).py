958f0eda05f71c511f38ac4454e370a43606035b
redahbch <redahbch45@gmail.com>
2025-10-19 11:08:06 +0200

    Actvite4(TAF).py

--- Actvite4(TAF).py
+++ Actvite4(TAF).py
@@ -1,6 +1,8 @@
 with open("Tabledemultiplication.txt", "w+") as file:
     for i in range(1,10):
+        file.write(f"La table de multiplication de {i}\n")
         for j in range(1,10):
             file.write(f"{i} x {j} = {i * j}\n")
         file.write("\n")  
+
 
